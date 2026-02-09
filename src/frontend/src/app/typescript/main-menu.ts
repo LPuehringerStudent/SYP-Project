ee91ec1c3b63c06564f0617bba9d05269c497612
David-Fruehwirt <d.fruehwirt@students.htl-leonding.ac.at>
2026-02-09 09:14:05 +0100

    done with main menu interfaces

---
+++ src/frontend/src/app/typescript/main-menu.ts
@@ -0,0 +1,27 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: '../app.html',
+  styleUrls: ['../app.css'],
+  standalone: true
+})
+export class MainMenuComponent {
+  sidebarOpen = false;
+  settingsOpen = false;
+  loginOpen = false;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  toggleSettings() {
+    this.settingsOpen = !this.settingsOpen;
+    this.loginOpen = false;
+  }
+
+  toggleLogin() {
+    this.loginOpen = !this.loginOpen;
+    this.settingsOpen = false;
+  }
+}
